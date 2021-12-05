ec08e772a613ccc0a8d6cd98d89cb608e7a1923b
ngv <funkycatz@yandex.ru>
2021-12-06 00:19:43 +0300

    day 4 - debug info

--- day4/BingoMatrix.cs
+++ day4/BingoMatrix.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace day4
@@ -11,14 +12,14 @@ namespace day4
             public bool Checked {get; set;}
         }
 
-/*
-0 0  0  0  0  0  <- X, Y axes are for checked numbers count
-0 1  33 12 55 13
-0 3  8  13 66 35
-0 5  9  99 21 67
-0 6  10 81 76 98
-0 11 44 32 90 42
-*/
+        /*
+        0 0  0  0  0  0  <- X, Y axes are for checked numbers count
+        0 1  33 12 55 13
+        0 3  8  13 66 35
+        0 5  9  99 21 67
+        0 6  10 81 76 98
+        0 11 44 32 90 42
+        */
 
         private BingoEntity[,] _table;
 
@@ -26,9 +27,47 @@ namespace day4
 
         private const int WiningResult = 5;
 
-        public BingoMatrix()
+        public BingoMatrix(string rawData)
         {
+            if(string.IsNullOrWhiteSpace(rawData))
+                throw new ArgumentException(rawData);
+
             _table = new BingoEntity[6, 6];
+
+            BuildBingoMatrix(rawData);
+        }
+
+        /* rawData exmpl
+        3 15  0  2 22
+        9 18 13 17  5
+        19  8  7 25 23
+        20 11 10 24  4
+        14 21 16 12  6
+
+        */
+        private void BuildBingoMatrix(string rawData){
+            string[] rows = rawData.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+            for (var i = 1; i < 6; i++)
+            {
+                int[] numbers = rows[i - 1].Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select( item => int.Parse(item))
+                    .ToArray();
+
+                for (var j = 1; j < 6; j++)
+                {
+                    _table[i, j].Number = numbers[j - 1];
+                    _table[i, j].Checked = false;
+                }    
+            }
+        }
+
+        public void PrintDebug(){
+            for(int x = 1; x < 6; ++x) {
+                for(int y = 1; y < 6; ++y) {
+                    Console.Write($"{_table[x,y].Number} ");                      
+                }
+                Console.WriteLine();
+            }
         }
 
         public void CheckIfFound(int number)
@@ -48,22 +87,15 @@ namespace day4
             }
         }
 
-        public IEnumerable<int> GetCheckedNumbers(){
+        public IEnumerable<int> GetNumbers(bool bingo)
+        {
             for(int x = 1; x < 6; ++x) {
                 for(int y = 1; y < 6; ++y) {
-                   if(_table[x,y].Checked)
+                   if(_table[x,y].Checked == bingo)
                         yield return _table[x,y].Number;                    
                 }
             }
         }
 
-        public IEnumerable<int> GetUncheckedNumbers(){
-            for(int x = 1; x < 6; ++x) {
-                for(int y = 1; y < 6; ++y) {
-                   if(!_table[x,y].Checked)
-                        yield return _table[x,y].Number;                    
-                }
-            }
-        }
     }
 }
--- day4/Program.cs
+++ day4/Program.cs
@@ -6,7 +6,15 @@ namespace day4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string rawData = @"
+        3 15  0  2 22
+        9 18 13 17  5
+        19  8  7 25 23
+        20 11 10 24  4
+        14 21 16 12  6";
+        BingoMatrix matrix = new BingoMatrix(rawData);
+
+            matrix.PrintDebug();
         }
     }
 }
